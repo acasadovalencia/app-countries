ea30880d0ed733fe3c4b2ed1951095438c44c914
Antonio Casado Valencia <acasadovalencia.dev@gmail.com>
2024-10-11 13:47:58 +0200

    fetch running

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,8 +1,28 @@
+import { useEffect, useState } from 'react'
 import { SearchInput } from '../../components/Header/SearchInput/SearchInput'
 import './Main.css'
 
 export const Main = ()=>{
 
+    const [ countries , setCountries] = useState([])
+    
+    const getCountries = async ()=>{
+        let controller = new AbortController()
+        let options = {
+            method: 'get',
+            signal: controller.signal,
+        }
+
+        await fetch('https://restcountries.com/v3.1/all' , options)
+        .then(res => res.json())
+        .then( data => setCountries(data))
+        .catch(err => console.log(err.message))
+        .finally(()=>controller.abort())
+    }
+
+    useEffect( ()=>{
+        getCountries()
+    }, [])
 
     return(
         <>
