b57d6c0ae9b51bdbdfea05e79ccdefb9e1921a71
Antonio Casado Valencia <acasadovalencia.dev@gmail.com>
2024-10-13 16:58:32 +0200

    countryLi done

--- src/components/CountryLi/CountryLi.jsx
+++ src/components/CountryLi/CountryLi.jsx
@@ -6,32 +6,35 @@ export const CountryLi = (props)=>{
 
     return(
         <>
-        {countries.length != 0 && countries.slice( 0 , 10).map( eachCountry => 
-                    <li key={eachCountry.cnn3}>
+        {countries.length != 0 && countries.slice( 0 , 8).map( eachCountry => 
+                    <li key={eachCountry.cnn3} className='Countries-li'>
                         <picture className="Countries-picture">
-                            <img src={eachCountry.flags.svg} alt="" className="Countries-img" />
+                            <img src={eachCountry.flags.svg} alt="" className="Countries-img" loading='lazy' />
                         </picture>
-                        <h2 className="Countries-h2">{eachCountry.name.common}</h2>
-                        <ul className="Country-ul">
-                            <li className="Country-li">
-                                <div className="Details-wrapper">
-                                    <h3 className="Country-h3">Population:</h3>
-                                    <span className="Country-span">{eachCountry.population}</span>
-                                </div>
-                            </li>
-                            <li className="Country-li">
-                                <div className="Details-wrapper">
-                                    <h3 className="Country-h3">Region:</h3>
-                                    <span className="Country-span">{eachCountry.region}</span>
-                                </div>
-                            </li>
-                            <li className="Country-li">
-                                <div className="Details-wrapper">
-                                    <h3 className="Country-h3">Capital:</h3>
-                                    <span className="Country-span">{eachCountry.capital}</span>
-                                </div>
-                            </li>
+                        <div className="Country-wrapper">
+                            <h2 className="Country-h2">{eachCountry.name.common}</h2>
+                            <ul className="Country-ul">
+                                <li className="Country-li">
+                                    <div className="Details-wrapper">
+                                        <h3 className="Country-h3">Population:</h3>
+                                        <span className="Country-span">{eachCountry.population}</span>
+                                    </div>
+                                </li>
+                                <li className="Country-li">
+                                    <div className="Details-wrapper">
+                                        <h3 className="Country-h3">Region:</h3>
+                                        <span className="Country-span">{eachCountry.region}</span>
+                                    </div>
+                                </li>
+                                <li className="Country-li">
+                                    <div className="Details-wrapper">
+                                        <h3 className="Country-h3">Capital:</h3>
+                                        <span className="Country-span">{eachCountry.capital}</span>
+                                    </div>
+                                </li>
                         </ul>
+                        </div>
+                        
                     </li>
                 )}
         </>
