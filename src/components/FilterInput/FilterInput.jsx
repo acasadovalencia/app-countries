11345aa364d7bb3d6fc94bd300f6c97b04cfc7b9
Antonio Casado Valencia <acasadovalencia.dev@gmail.com>
2024-10-13 17:55:33 +0200

    filter options done

--- src/components/FilterInput/FilterInput.jsx
+++ src/components/FilterInput/FilterInput.jsx
@@ -1,15 +1,22 @@
+import { useContext, useEffect } from 'react'
 import './FilterInput.css'
+import { CountriesContext } from '../../Context/Context'
 
 export const FilterInput = ()=>{
+
+    const { countries } = useContext(CountriesContext)
+
+    const regions = [... new Set(countries.map(eachCountry => eachCountry.region))]
+
 
     return(
         <>
         <form className="Filter-form">
             <select className='Filter-input' name="regions" id="regions">
                 <option value="">Filter by region</option>
-                <option value="europe">Europe</option>
-                <option value="asia">Asia</option>
-
+                {regions.map(eachRegion => 
+                    <option value={eachRegion}>{eachRegion}</option>
+                )}
             </select>
         </form>
         </>
